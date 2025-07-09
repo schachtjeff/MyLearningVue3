fdb586095bc0d9469d489f7a481ac735b7320218
Jeffrey Schachtsick <schachtj46@gmail.com>
2025-07-09 14:09:30 -0700

    methods concept

--- Section2_CoreConcepts/app.js
+++ Section2_CoreConcepts/app.js
@@ -4,6 +4,17 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course and learn Vue!',
             vueLink: 'https://vuejs.org/'
         };
+    },
+    //Methods allows you to execute when something occurs like a button click.
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learn Vue!';
+            } else {
+                return 'Master Vue!';
+            }
+        }
     }
 });
 
