3d8e2da1d6dc73633b4a8ea94fab811857afa662
Jeffrey Schachtsick <schachtj46@gmail.com>
2025-07-10 20:32:27 -0700

    event modifiers and initial locks

--- section2/2-EventBinding/app.js
+++ section2/2-EventBinding/app.js
@@ -2,18 +2,25 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
     addCounter(num) {
       this.counter = this.counter + num;
     },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
     subtractCounter(num) {
       this.counter = this.counter - num;
     },
     setName(username) {
       this.name = username.target.value;
+    },
+    submitForm() {
+      alert('Submitted!');
     }
   }
 });
