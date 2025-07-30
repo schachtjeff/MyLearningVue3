43d2fc9c8f78ea201a0f5add0d58cb5c40fb0f2b
Jeffrey Schachtsick <schachtj46@gmail.com>
2025-07-30 15:42:41 -0700

    add css classes dynamically

--- section2/2-EventBinding/styling/app.js
+++ section2/2-EventBinding/styling/app.js
@@ -2,19 +2,19 @@ const app = Vue.createApp({
     data() {
          return {
             //Keep track of html div
-            //boxASelected: false,
-            //boxBSelected: false,
-            //boxCSelected: false
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false
         }; 
     }, 
     methods: {
         boxSelected(box) {
             if (box === 'A') {
-                this.boxASelected = true;
+                this.boxASelected = !this.boxASelected;
             } else if (box === 'B') {
-                this.boxBSelected = true;
+                this.boxBSelected = !this.boxBSelected;;
             } else if (box === 'C') {
-                this.boxCSelected = true;
+                this.boxCSelected = !this.boxCSelected;;
             } 
         }
     }
