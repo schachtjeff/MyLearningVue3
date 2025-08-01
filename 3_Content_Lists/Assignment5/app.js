1a6717d4e591056ad0fa976eb93fc5eba54dee15
Jeffrey Schachtsick <schachtj46@gmail.com>
2025-08-01 11:15:24 -0700

    doneish

--- 3_Content_Lists/Assignment5/app.js
+++ 3_Content_Lists/Assignment5/app.js
@@ -2,12 +2,21 @@ const app = Vue.createApp({
     data() {
         return {
             taskValue: '',
-            tasks: []
+            tasks: [],
+            taskListVisible: true
         };
+    },
+    computed: {
+        hideShowMessage() {
+            return this.taskListVisible ? 'Hide List' : 'Show List';
+        }
     },
     methods: {
         addTask() {
             this.tasks.push(this.taskValue);
+        },
+        toggleTaskVisibility() {
+            this.taskListVisible = !this.taskListVisible;
         }
     }
 });
