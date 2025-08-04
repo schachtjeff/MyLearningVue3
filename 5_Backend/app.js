3b1ddcb02f23c954f341ea804bb00c39f835d436
Jeffrey Schachtsick <schachtj46@gmail.com>
2025-08-04 11:48:51 -0700

    deep dive notes

--- 5_Backend/app.js
+++ 5_Backend/app.js
@@ -16,3 +16,37 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+// JavaScript is not REactive
+let message = 'Hello!';
+let theMessage = message + ' World!';
+console.log(theMessage);
+//prints in console 'Hello World!'
+
+message = 'Hello!!!!'
+console.log(theMessage)
+//prints in console 'Hello World!'
+//not reactive, vue works different
+
+// Handler concept
+
+const data = {
+  message: 'Hello!',
+  newerMessage: 'Hello World!'
+};
+
+const handler = {
+  set(target, key, value) {
+    console.log(target);
+    console.log(key);
+    console.log(value);
+    if (key === 'message') {
+      target.newerMessage = value + ' World!'
+    }
+    target.message = value;
+  }
+};
+
+const proxy = new Proxy (data, handler);
+proxy.message = 'Hello!!!';
+console.log(proxy.newerMessage);
