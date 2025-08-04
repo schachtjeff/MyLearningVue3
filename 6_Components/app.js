18765eb1c92c1073053529a71e018b0b1d0486ef
Jeffrey Schachtsick <schachtj46@gmail.com>
2025-08-04 15:03:37 -0700

    template and components

--- 6_Components/app.js
+++ 6_Components/app.js
@@ -19,4 +19,35 @@ const app = Vue.createApp({
     }
 });
 
+app.component('friend-contact', {
+    template: `
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails()">
+          {{ detailsAreVisible ? 'Hide' : 'Show' }}  Details
+        </button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+    `,
+    data() {
+        return { 
+            detailsAreVisible: false,
+            friend: {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '012345678991',
+                    email: 'manuel@localhost.com'
+                }
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+});
+
 app.mount('#app');
